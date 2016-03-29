dbdf7030ac66be5ee3ea4a5c9bfb22c2fb91d3cf
Hackerl <liupan052997@gmail.com>
2016-03-29 14:03:19 +0800

    blog

---
+++ web.service/FOUR/blog.py
@@ -0,0 +1,293 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import torndb
+import tornado.httpserver
+import tornado.web
+import tornado.ioloop
+import os.path
+import markdown
+import ConfigParser
+import re
+from model import Article, Label, Auth, Search
+from component import Paginator
+from tornado.options import define, options
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r'/', HomeHandler),
+            (r'/article/(\d+)', ArticleHandler),
+            (r"/about", AboutHandler),
+            (r"/music", MusicHandler),
+            (r"/prose", ProseHandler),
+            (r"/program", ProgramHandler),
+            (r'/createArticle', CreateArticleHandler),
+            (r'/preview', PreviewHandler),
+            (r'/article/edit/(\d+)', EditArticleHandler),
+            (r'/article/update/(\d+)', UpdateArticleHandler),
+            (r'/login', LoginHandler),
+            (r'/logout', LogoutHandler),
+            (r'/auth', AuthHandler),
+            (r'/page/(\d+)', ArticleListHandler),
+        ]
+        settings = dict(
+            static_path=os.path.join(os.path.dirname(__file__), 'static'),
+            template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+            autoescape=None,
+            xsrf_cookies=True,
+            cookie_secret='6de683f6e8f038f62863fe27a17573e5',
+            login_url='/login',
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+        self.db = torndb.Connection(
+            host=options.mysql_host, database=options.mysql_database,
+            user=options.mysql_user, password=options.mysql_password
+        )
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        return self.application.db
+
+    def write_error(self, status_code, **kwargs):
+        if status_code == 400:
+            error = "400: Bad Request"
+            self.render('error.html', error=error, home_title=options.home_title)
+        if status_code == 405:
+            error = "405: Method Not Allowed"
+            self.render('error.html', error=error, home_title=options.home_title)
+        if status_code == 404:
+            error = "404: Page Not Found"
+            self.render('error.html', error=error, home_title=options.home_title)
+
+    def get_current_user(self):
+        return self.get_secure_cookie('user')
+
+    def isAdmin(self):
+        if self.get_secure_cookie('user'):
+            return True
+        else:
+            return False
+
+class AboutHandler(BaseHandler):
+    def get(self):
+        self.render("about.html")
+class MusicHandler(BaseHandler):
+    def get(self):
+        self.render("music.html")
+class ProseHandler(BaseHandler):
+    def get(self):
+        p = Paginator(Article.all(self.db,'prose'), 5)
+        page = p.page(1)
+        isAdmin = self.isAdmin()
+        label_list = Label.group(self.db)
+        self.render('prose.html', articles=page.object_list, label_list=label_list,
+                isAdmin=isAdmin, page=page)
+class ProgramHandler(BaseHandler):
+    def get(self):
+        p = Paginator(Article.all(self.db,'program'), 5)
+        page = p.page(1)
+        isAdmin = self.isAdmin()
+        label_list = Label.group(self.db)
+        self.render('program.html', articles=page.object_list, label_list=label_list,
+                isAdmin=isAdmin, page=page)
+class HomeHandler(BaseHandler):
+    def get(self):
+        p = Paginator(Article.all(self.db), 5)
+        page = p.page(1)
+        isAdmin = self.isAdmin()
+        label_list = Label.group(self.db)
+        self.render('index.html', articles=page.object_list, label_list=label_list,
+                isAdmin=isAdmin, page=page)
+
+
+class ArticleListHandler(BaseHandler):
+    def get(self, pageId):
+        p = Paginator(Article.all(self.db), 5)
+        page = p.page(int(pageId))
+        isAdmin = self.isAdmin()
+        label_list = Label.group(self.db)
+        self.render('index.html', articles=page.object_list, label_list=label_list,
+                isAdmin=isAdmin, page=page)
+
+
+class ArticleHandler(BaseHandler):
+    def get(self, id):
+        article = Article.get(self.db, id)
+        if article is None:
+            error = '404: Page Not Found'
+            self.render('error.html', error=error, home_title=options.home_title)
+        else:
+            isAdmin = self.isAdmin()
+            label_list = Label.group(self.db)
+            self.render('article.html', article=article, label_list=label_list, isAdmin=isAdmin)
+
+
+class PreviewHandler(BaseHandler):
+    @tornado.web.authenticated
+    def post(self):
+        title = self.get_argument('title')
+        sort=self.get_argument('sort')
+        content_md = self.get_argument('content')
+        pattern = r'\[[^\[\]]+\]'
+        labels = re.findall(pattern, self.get_argument('labels'))
+        content_html = markdown.markdown(content_md, ['codehilite'])
+
+        article = {}
+        article['sort']=sort
+        article['title'] = title
+        article['content_html'] = content_html
+        article['labels'] = [{'detail': label[1:-1].strip()} for label in labels]
+
+        data = {}
+        data['sort']=sort
+        data['title'] = title
+        data['labels'] = self.get_argument('labels')
+        data['content'] = content_md
+
+        self.render('preview.html', article=article, data=data,
+                user=options.user, photo=options.photo)
+
+
+class EditArticleHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self, id):
+        article = Article.get(self.db, id)
+        if article is None:
+            error = '404: Page Not Found'
+            self.render('error.html', error=error, home_title=options.home_title)
+        else:
+            labels = ' '.join(map(lambda item: '[' + item['detail'] + ']', article['labels']))
+            self.render('editArticle.html', article=article, labels=labels)
+
+
+class UpdateArticleHandler(BaseHandler):
+    @tornado.web.authenticated
+    def post(self, id):
+        title = self.get_argument('title')
+        content_md = self.get_argument('content')
+        sort = self.get_argument('sort')
+        print(sort)
+        pattern = r'\[[^\[\]]+\]'
+        labels = re.findall(pattern, self.get_argument('labels'))
+        content_html = markdown.markdown(content_md, ['codehilite'])
+
+
+        Article.update(self.db, id, title, content_md, content_html,sort)
+        try:
+            Label.deleteAll(self.db, id)
+            for label in labels:
+                detail = label[1:-1].strip()
+                Label.create(self.db, id, detail)
+
+            self.redirect('/article/' + id, permanent=True)
+        except:
+            error = "The post data invalid"
+            self.render('error.html', error=error, home_title=options.home_title)
+
+
+class CreateArticleHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.render('createArticle.html')
+
+    @tornado.web.authenticated
+    def post(self):
+        title = self.get_argument('title')
+        content_md = self.get_argument('content')
+        sort = self.get_argument('sort')
+
+        pattern = r'\[[^\[\]]+\]'
+        labels = re.findall(pattern, self.get_argument('labels'))
+        content_html = markdown.markdown(content_md, ['codehilite'])
+        print(title,content_md,sort,labels,content_html)
+        try:
+            article_id = Article.create(self.db, title, content_md, content_html,sort)   
+            for label in labels:
+                detail = label[1:-1].strip()
+                Label.create(self.db, article_id, detail)
+            self.redirect('/', permanent=True)
+        except:
+            error = "The post data invalid"
+            self.render('error.html', error=error, home_title=options.home_title)
+
+
+class SearchHandler(BaseHandler):
+    def get(self):
+        key = self.get_argument('key', '').strip()
+        pageId = int(self.get_argument('page', '1'))
+        if len(key) == 0:
+            results = Article.all(self.db)
+        else:
+            results = Search.all(self.db, key)
+        p = Paginator(results, 5)
+        page = p.page(pageId)
+        isAdmin = self.isAdmin()
+        label_list = Label.group(self.db)
+
+        self.render('search.html', articles=page.object_list, label_list=label_list,
+                isAdmin=isAdmin, page=page, home_title=options.home_title,
+                user=options.user, photo=options.photo)
+
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        next = self.get_argument('next', '/')
+        self.render('login.html', next=next)
+
+
+class LogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie('user')
+        self.redirect('/', permanent=True)
+
+
+class AuthHandler(BaseHandler):
+    def post(self):
+        username = self.get_argument("username")
+        password = self.get_argument("password")
+
+        try:
+            if self.validate(username) is None:
+                raise
+
+            if Auth.authenticate(self.db, username, password):
+                self.set_secure_cookie("user", "admin", expires_days=1)
+                self.redirect(self.get_argument('next', '/'), permanent=True)
+            else:
+                error = "Authentication failure"
+                self.render('error.html', error=error, home_title=options.home_title)
+        except:
+            error = "The user not exists"
+            self.render('error.html', error=error, home_title=options.home_title)
+
+    def validate(self, username):
+        regex = re.compile(r'^[\w\d]+$')
+        return regex.match(username)
+
+
+def main():
+    config = ConfigParser.ConfigParser()
+    config.read('blog.cfg')
+    mysql = dict(config.items('mysql'))
+    blog = dict(config.items('blog'))
+
+    define("port", default=int(blog['port']), type=int)
+    define("mysql_host", default="127.0.0.1:3306")
+    define("mysql_database", default=mysql['database'])
+    define("mysql_user", default=mysql['user'])
+    define("mysql_password", default=mysql['password'])
+    define("blog_hostname", default=blog['hostname'])
+    define("user", default=blog['user'])
+    define("home_title", default=blog['home_title'])
+    define("photo", default=blog['photo'])
+
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+    main()
---
+++ web.service/FOUR/model.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import hashlib
+import time
+
+class Article(object):
+    @classmethod
+    def all(cls, db,sort=None):
+        if sort:
+            articles = db.query("SELECT * FROM article where sort=%s ORDER BY time DESC",sort)
+            for article in articles:
+                article['labels'] = Label.all(db, article.id)
+            return articles
+        else:
+            articles = db.query("SELECT * FROM article ORDER BY time DESC")
+            for article in articles:
+                article['labels'] = Label.all(db, article.id)
+            return articles            
+
+    @classmethod
+    def get(cls, db, id):
+        article = db.get('SELECT * FROM article WHERE id = %s', id)
+        if article is None:
+            return article
+        else:
+            article['labels'] = Label.all(db, article.id)
+            return article
+
+    @classmethod
+    def create(cls, db, title, content_md, content_html,sort):
+        return db.execute('insert into article (title, content_md, content_html,sort,time) \
+               values("%s", "%s", "%s","%s",CURRENT_TIMESTAMP())'%(title, content_md, content_html,sort))
+    @classmethod
+    def update(cls, db, id, title, content_md, content_html,sort):
+        db.execute("UPDATE article set title=%s, content_md=%s, \
+                content_html=%s,sort=%s WHERE id=%s",title, content_md, content_html,sort,id)
+
+    @classmethod
+    def totalNumber(cls, db):
+        return db.execute_rowcount('SELECT * FROM article')
+
+
+class Label(object):
+    @classmethod
+    def all(cls, db, article_id):
+        return db.query('SELECT detail FROM label \
+                WHERE article_id = %s', article_id)
+
+    @classmethod
+    def create(cls, db, article_id, detail):
+        db.execute('INSERT INTO label (article_id, detail) \
+                VALUES ("%s", "%s")'%(article_id, detail))
+
+    @classmethod
+    def deleteAll(cls, db, article_id):
+        db.execute('DELETE FROM label WHERE article_id=%s'%article_id)
+
+    @classmethod
+    def group(cls, db):
+        return db.query('SELECT detail, count(*) AS number \
+                FROM label GROUP BY detail ORDER BY number DESC')
+
+
+class Auth(object):
+    @classmethod
+    def authenticate(cls, db, username, password):
+        hashPassword = db.get('SELECT password FROM auth \
+                WHERE username = %s', username)['password']
+        return hashlib.md5(password).hexdigest() == hashPassword
+
+
+class Search(object):
+    @classmethod
+    def all(cls, db, key):
+        results = db.query('SELECT a.id, a.title, a.content_md, a.content_html, a.time \
+                FROM article a, label l \
+                WHERE a.id = l.article_id AND l.detail = %s \
+                ORDER BY time DESC', key)
+        for result in results:
+            result['labels'] = Label.all(db, result.id)
+        return results
